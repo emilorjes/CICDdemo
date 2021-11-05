5d98b22cb656817948947c8542b991edc0b55a4f
Emil Örjes <emil.orjes@hotmail.com>
2021-11-05 11:28:02 +0100

    Added "add" method

--- CICDdemo/Program.cs
+++ CICDdemo/Program.cs
@@ -7,6 +7,12 @@ namespace CICDdemo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(Add(5,8));
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
