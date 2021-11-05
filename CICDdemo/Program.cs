ef40405e5426b2dc561493a1ba8affc90ec0f17e
Emil Örjes <emil.orjes@hotmail.com>
2021-11-05 11:21:17 +0100

    Add project files.

---
+++ CICDdemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CICDdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
