8f9d88fb4d5b24b0ff46514ff0268ddda90ab787
Emil Örjes <emil.orjes@hotmail.com>
2021-11-05 11:31:59 +0100

    Added "Sub" method

--- CICDdemo/Program.cs
+++ CICDdemo/Program.cs
@@ -8,11 +8,17 @@ namespace CICDdemo
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(Add(5,8));
+            Console.WriteLine(Sub(10,5));
         }
 
         public static int Add(int a, int b)
         {
             return a + b;
+        }
+
+        public static int Sub(int a, int b)
+        {
+            return a - b;
         }
     }
 }
